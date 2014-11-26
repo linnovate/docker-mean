b0ce9baee07cfcd083ee300e99c573e46ea3dabf
brownman <brownman556@gmail.com>
2014-11-26 13:31:43 +0200

    Update test_repo.sh

--- test_repo.sh
+++ test_repo.sh
@@ -1,21 +1,5 @@
-#documentation:
-#https://docs.docker.com/examples/nodejs_web_app/
+#!/bin/bash
+source config.cfg
 
-commander(){
-  local args=( "$@" )
-  local cmd="${args[@]}"
-  echo 1>&2 "[cmd] $cmd"
-  eval "$cmd"
-}
-
-
-name='docker-node-hello-cube'
-
-
-
-commander git clone https://github.com/cwarden/$name.git
-commander cd $name
-commander  sudo docker build .
-commander  sudo docker build -t $LOGNAME/$name .
-commander sudo docker images
-commander sudo docker run $LOGNAME/$name
+cd REPO/$machine_name
+./run.sh
